2a190cef094f68796607ca9c1c4f2a093f1e3e5c
cypher-oo <pritirai8732@gmail.com>
2020-11-11 18:12:59 +0530

    version 1

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import { NavLink } from "react-router-dom";
+import web from "../src/images/img2.jpeg";
+import Common from "./Common";
+
+const Home = () => {
+  return (
+    <>
+     <Common 
+      name="Grow your business with" 
+      imgsrc={web}
+      visit="/service"
+      btname="Get Started"
+      />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/Sdata.jsx
@@ -0,0 +1,40 @@
+import web from "../src/images/img3.jpeg";
+import app from "../src/images/app.jpeg";
+import android from "../src/images/android.png";
+import digital from "../src/images/digital.png";
+import software from "../src/images/software.jpeg";
+import marketing from "../src/images/marketing.png";
+
+const Sdata = [
+  {
+    imgsrc: web,
+    title: "web development",
+ },
+ {
+  imgsrc: app,
+  title:"App development",
+},
+{
+  imgsrc: software,
+  title: "Software development",
+
+},
+{
+  imgsrc: digital,
+  title: "Digital Marketing",
+},
+
+{
+  imgsrc: android,
+  title: "Android development",
+},
+{
+  imgsrc: marketing,
+  title: "Marketing",
+},
+
+
+
+];
+
+export default Sdata;
